169d0d6bc47d3734521b516e4c144305f4f2ffba
Karan Khatri <karankhatri535@gmail.com>
2023-02-26 15:41:18 +0530

    done with the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Card from './components/Card';
+import Data from './Data';
 
 const App = () => {
+
+  const cards = Data.map(item => {
+    return(
+      <Card 
+        key = {item.id}
+        item = {item}
+      />
+    )
+  })
+
   return (
     <div>
       <Navbar />
-      <Card />
+      {cards}
     </div>
   );
 };
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import Mountain from '../images/mountain.png';
 import Mark from '../images/mark.png';
 
-const Card = () => {
+const Card = (props) => {
   return (
     <section className='card'>
-        <img src={Mountain} className="card--img"/>
+        <img src={props.item.coverImg} className="card--img"/>
         <div className='card--stats'>
             <img src={Mark} />
-            <span className='card--location'>JAPAN </span>
+            <span className='card--location'>{props.item.location} </span>
             <span className='card--google'>View on Google Maps</span>
-            <p className='card--heading'>Mount Fuji</p>
-            <p className='card--date'>12 Jan, 2021 - 24 Jan, 2021</p>
-            <p className='card--para'>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists</p>
+            <p className='card--heading'>{props.item.placeName}</p>
+            <p className='card--date'>{props.item.date}</p>
+            <p className='card--para'>{props.item.description}</p>
         </div>
     </section>
   );
